ffa8d2c3d3b877d9fb39525418691acd7bfff06f
Lei <xuliulei666@163.com>
2022-01-20 21:24:18 +0800

    update login

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -682,6 +682,7 @@ export default {
             }).catch(err => {
               console.log(err)
               this.$message.error(err.msg)
+              this.submitLoginLoadingStatus = false
             })
 
           } else {
